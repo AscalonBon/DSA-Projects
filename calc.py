057e95788046c1c0b92a8c8d7225640d9156317f
AscalonBon <kirt566bon@gmail.com>
2024-10-08 00:19:18 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,48 +1,73 @@
-next = 'y'
-while next:
-    num1 = float(input('First number: '))
-    num2 = float(input('Second number: '))
+class Calculator:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.op = 0
+        self.proceed = 'Y'
 
-    print('1. Addition / 2. Subtraction / 3. Multiplication / 4. Division / 5. Reset input')
-    op = int(input('Operation: '))
+    def add(self, x, y):
+        return x + y
 
-    #format for solving
-    def add(num1, num2):
-        return num1 + num2
+    def sub(self, x, y):    
+        return x - y
 
-    def sub(num1, num2):
-        return num1 - num2
+    def multiply(self, x, y):
+        return x * y
 
-    def mult(num1, num2):
-        return num1 * num2
+    def divide(self, x, y):
+        if y == 0:
+            return 'Cannot divide by zero!'
+        return x / y
 
-    def div(num1, num2):    
-        return num1 / num2
+    def operation(self):
+        if self.op == 1:
+            print('The answer is:', self.add(self.x, self.y))
 
-    #solving conditions
-    if op  == 1:
-        print('The answer is:', (add(num1, num2)))
+        elif self.op == 2:
+            print('The answer is:', self.sub(self.x, self.y))
 
-    elif op  == 2:
-        print('The answer is:', (sub(num1, num2))) 
+        elif self.op == 3:
+            print('The answer is:', self.multiply(self.x, self.y))
 
-    elif op  == 3:
-        print('The answer is:', (mult(num1, num2))) 
+        elif self.op == 4:
+            if self.y == 0:
+                print('Cannot divide by zero!')
+            else:
+                print('The answer is:', self.divide(self.x, self.y))
 
-    elif op  == 4:
-        if num2 == 0:
-            print('This cannot be')
-        else:
-            print('The answer is:', (div(num1, num2))) 
-    
-    elif op == 5:
-        print('Input new numbers')
-        continue
+    def next_calc(self):
+        while True:
+            self.proceed = input('Do you want to continue? (Y/N): ').upper()
+            if self.proceed == 'Y':
+                return True
+            elif self.proceed == 'N':
+                return False
+            else:
+                print("Please choose Y or N.")
 
-    else:
-        print('Invalid operation. Please select a valid operation (1-5)')
-    
-    next = input('Next calculation? (Y/N): ').lower()
-    if next == 'n':
-        break
-    
+    def run(self):
+        while self.proceed.upper() == 'Y':
+            while True:
+                try:
+                    print('Welcome to Calculator!')
+                    print('1. Addition / 2. Subtraction / 3. Multiplication / 4. Division: ')
+                    self.op = int(input('Choose an operation: '))
+                    if self.op in [1, 2, 3, 4]:
+                        break
+                    else:
+                        print('Please choose a number from 1 to 4.')
+                except ValueError:
+                    print('Please choose a number from 1 to 4.')
+
+            self.x = float(input('First Number: '))
+            self.y = float(input('Second Number: '))
+
+            self.operation()
+
+            if not self.next_calc():
+                print('Goodbye!')
+                break
+
+# Create an instance of Calculator and run it
+calc = Calculator()
+calc.run()
